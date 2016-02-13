e05a2d5ea9626b6c1d2c18ef02bac1b3f10eadf6
atmalmer18 <atmalmer23@gmail.com>
2016-02-13 23:42:04 +0800

    Heavy documentation on code; initialized functions to be used.

---
+++ library.py
@@ -0,0 +1,33 @@
+# Author: Almer T. Mendoza
+# CMSC 128 AB - 6L
+
+## A LIBRARY THAT IMPLEMENTS THE FOLLOWING #
+
+## 1. Converting number to words 
+## 2. Converting words to numbers
+## 3. Converting words to currency
+## 4. Inserting delimiter to a number
+
+### numToWords (number)
+### Accepts a whole number and outputs an equivalent in word form
+def numToWords:
+    #insert code here
+
+### wordsToNum (string)
+### Accepts a number in word form and returns its equivalent in number format
+def wordsToNum:
+    #insert code here
+
+### wordsToCurrency (string, string)
+### Accepts two strings (number in word format, [JPY|USD|PHP]) and outputs the number with the currency
+def wordsToCurrency:
+    #insert code here
+
+### numberDelimited (number, character, number)
+### Accepts a number, a character / delimiter and another number
+### First number will be used as the number itself to be printed out 
+### The character will be the delimiter for the first number
+### Last number will define the number of jumps for the delimiter to be placed
+def numberDelimited:
+    #insert code here
+    
