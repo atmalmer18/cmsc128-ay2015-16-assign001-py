a9de979257515ce4ab869a723e95a5def8b942c4
atmalmer18 <atmalmer23@gmail.com>
2016-02-14 06:04:30 +0800

    moved all function definitions at the top of each block for readability

--- library.py
+++ library.py
@@ -12,6 +12,43 @@ import sys
 ### numToWords (number)
 ### Accepts a whole number and outputs an equivalent in word form
 def numToWords(num):
+	def getTens (y):
+		return {
+			10: "ten",
+			11: "eleven",
+			12: "twelve",
+			13: "thirteen",
+			14: "fourteen",
+			15: "fifteen",
+			16: "sixteen",
+			17: "seventeen",
+			18: "eighteen",
+			19: "nineteen",
+		}.get(y)
+	def switchNum (x):
+		return {
+			10**6: "million",
+			10**3: "thousand",
+			10**2: "hundred",
+			20: "twenty",
+			30: "thirty",
+			40: "fourty",
+			50: "fifty",
+			60: "sixty",
+			70: "seventy",
+			80: "eighty",
+			90: "ninety",
+			0: "zero",
+			1: "one",
+			2: "two",
+			3: "three",
+			4: "four",
+			5: "five",
+			6: "six",
+			7: "seven",
+			8: "eight",
+			9: "nine",
+		}.get(x, x)
 	output = ""
 	numbers = []
 	size = 0
@@ -38,43 +75,6 @@ def numToWords(num):
 		if num == 0:
 			break
 	for counter in range(0, size):
-		def getTens (y):
-			return {
-				10: "ten",
-				11: "eleven",
-				12: "twelve",
-				13: "thirteen",
-				14: "fourteen",
-				15: "fifteen",
-				16: "sixteen",
-				17: "seventeen",
-				18: "eighteen",
-				19: "nineteen",
-			}.get(y)
-		def switchNum (x):
-			return {
-				10**6: "million",
-				10**3: "thousand",
-				10**2: "hundred",
-				20: "twenty",
-				30: "thirty",
-				40: "fourty",
-				50: "fifty",
-				60: "sixty",
-				70: "seventy",
-				80: "eighty",
-				90: "ninety",
-				0: "zero",
-				1: "one",
-				2: "two",
-				3: "three",
-				4: "four",
-				5: "five",
-				6: "six",
-				7: "seven",
-				8: "eight",
-				9: "nine",
-			}.get(x, x)
 		digit = numbers.pop()
 		if digit >= 10 and digit <= 19:
 			sys.stdout.write(switchNum(getTens(digit)) + " ")
@@ -89,11 +89,6 @@ def numToWords(num):
 ### wordsToNum (string)
 ### Accepts a number in word form and returns its equivalent in number format
 def wordsToNum(word):
-	output = []
-	final = 0;
-	hundreds = 0
-	thousands = 0
-	millions = 0
 	def switchNum (x):
 		return {
 			"hundred"	: 10**2,
@@ -131,6 +126,11 @@ def wordsToNum(word):
 			"eight"		: 8,
 			"nine"		: 9
 		}.get(x, 0)
+	output = []
+	final = 0;
+	hundreds = 0
+	thousands = 0
+	millions = 0
 	word = word.split(" ")
 	word.reverse()
 	for counter in range(0, len(word)):
@@ -151,11 +151,6 @@ def wordsToNum(word):
 ### wordsToCurrency (string, string)
 ### Accepts two strings (number in word format, [JPY|USD|PHP]) and outputs the number with the currency
 def wordsToCurrency(word, currency):
-	output = []
-	final = 0;
-	hundreds = 0
-	thousands = 0
-	millions = 0
 	def switchNum (x):
 		return {
 			"hundred"	: 10**2,
@@ -193,6 +188,11 @@ def wordsToCurrency(word, currency):
 			"eight"		: 8,
 			"nine"		: 9
 		}.get(x, 0)
+	output = []
+	final = 0;
+	hundreds = 0
+	thousands = 0
+	millions = 0
 	word = word.split(" ")
 	word.reverse()
 	for counter in range(0, len(word)):
@@ -230,4 +230,3 @@ def numberDelimited(num, delimiter, jump):
 	print(output)
 	
 
-	
